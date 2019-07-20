6426196753abe2507173fd42f913d98626f67228
Howard.Wang <idontwannarock@gmail.com>
2019-07-20 10:33:25 +0800

    add spring boot structure
    with web dependency only

---
+++ src/main/java/com/example/demoamposrestaurant/DemoAmposRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoamposrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAmposRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAmposRestaurantApplication.class, args);
+	}
+
+}
