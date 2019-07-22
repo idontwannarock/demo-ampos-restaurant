08f3c47d573a521ee59a807e049b64bd403de1c5
Howard.Wang <idontwannarock@gmail.com>
2019-07-22 18:52:23 +0800

    edit swagger config

--- src/main/java/com/example/demoamposrestaurant/SwaggerConfig.java
+++ src/main/java/com/example/demoamposrestaurant/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -18,7 +19,9 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("employee-api")
                 .apiInfo(apiInfo)
-                .select().paths(PathSelectors.any())
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.example.demoamposrestaurant"))
+                    .paths(PathSelectors.any())
                 .build();
     }
 
