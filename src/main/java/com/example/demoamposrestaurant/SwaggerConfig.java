c9b0d38fa9a867278f62369f64eeaf896b40d3f2
Howard Wang <idontwannarock@gmail.com>
2019-07-24 16:05:06 +0800

    edit Swagger path selector

--- src/main/java/com/example/demoamposrestaurant/SwaggerConfig.java
+++ src/main/java/com/example/demoamposrestaurant/SwaggerConfig.java
@@ -3,12 +3,12 @@ package com.example.demoamposrestaurant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.ant;
 
 @EnableSwagger2
 @Configuration
@@ -19,9 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("employee-api")
                 .apiInfo(apiInfo)
-                .select()
-                    .apis(RequestHandlerSelectors.basePackage("com.example.demoamposrestaurant"))
-                    .paths(PathSelectors.any())
+                .select().paths(ant("/api/**"))
                 .build();
     }
 
